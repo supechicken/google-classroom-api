5c4e45722369e1e2f90f649d7b8311579adea88e
supechicken <supechicken666@gmail.com>
2023-05-23 20:53:10 +0800

    user friendly

---
+++ google_auth.js
@@ -0,0 +1,86 @@
+const CLIENT_ID = '57261959261-qbph0j4o183o13dl4hco5ko97flla8oh.apps.googleusercontent.com',
+      API_KEY = 'AIzaSyBXewH_F63y74f4mGlRfQgazOqM4H7LR08',
+      DISCOVERY_DOC = 'https://classroom.googleapis.com/$discovery/rest';
+
+const SCOPES = [
+  'auth/classroom.courses.readonly',
+  'auth/classroom.student-submissions.me.readonly',
+  'auth/classroom.coursework.students',
+  'auth/classroom.courses.readonly',
+  'auth/classroom.course-work.readonly',
+  'auth/classroom.announcements.readonly'
+].reduce((acc, scope) => `${acc}https://www.googleapis.com/${scope} `, '');
+
+let tokenClient, gapiInited = false, gisInited = false;
+
+console.log('Scopes: ', SCOPES);
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+function gapiLoaded() {
+  console.log('gapi loaded');
+  gapi.load('client', async () => {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+  });
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  console.log('gis loaded');
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    prompt: '',
+    callback: undefined, // will be defined later
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    const btn = document.getElementById('authorize_button');
+    btn.innerText = 'Log in with Google account';
+    btn.removeAttribute('disabled');
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) throw (resp);
+    document.getElementById('signout_button').style.display = 'unset';
+    document.getElementById('authorize_button').style.display = 'none';
+    resolveGoogleLoginPromise();
+  };
+
+  tokenClient.requestAccessToken();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken('');
+    document.getElementById('content').innerText = '';
+    document.getElementById('authorize_button').style.display = 'unset';
+    document.getElementById('signout_button').style.display = 'none';
+  }
+}
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function addZeroPadding(num) {
+  return num.toString().padStart(2, '0');
+}
+
 function createHWProgressBar(percentage) {
   const progressBar = document.createElement('div'),
         completedBar = document.createElement('div');
@@ -68,3 +72,131 @@ function generateHWStatusBadge(status) {
 
   return badge;
 }
+
+(new Promise((resolve, _) => window.resolveGoogleLoginPromise = resolve)).then(async () => {
+  const detailsDiv = document.getElementById('details'),
+        progress = document.createElement('h3');
+
+  window.courseInfo = [];
+  detailsDiv.appendChild(progress);
+
+  console.log('Fetching course list...');
+  progress.innerText = 'Loading... Please wait (Fetching course list)';
+
+  const coursesResponse = await gapi.client.classroom.courses.list(),
+        courses = coursesResponse.result.courses.filter(c => c.courseState != 'ARCHIVED'); // ignore archived courses to save API quota
+
+  console.log('Fetching homework list...');
+  progress.innerText = 'Loading... Please wait (Fetching homework list)';
+
+  const coursesWorkBatch = gapi.client.newBatch();
+
+  courses.forEach(c => {
+    c.courseWork_request_id = coursesWorkBatch.add(gapi.client.classroom.courses.courseWork.list({ courseId: c.id }));
+  });
+
+  await coursesWorkBatch.then(batchResponse => {
+    for (const [id, response] of Object.entries(batchResponse.result)) {
+      const course = courses.filter(c => c.courseWork_request_id === id)[0];
+      course.courseWork = JSON.parse(response.body).courseWork;
+    }
+  })
+
+  console.log('Fetching homework details...');
+  progress.innerText = 'Loading... Please wait (Fetching homework details)';
+
+  const courses_and_hw = await Promise.all(courses.map(async course => {
+    const courseWork = course.courseWork;
+
+    if (courseWork) {
+      const submissionsBatch = gapi.client.newBatch();
+
+      courseWork.forEach(hw => {
+        // get submission states of each homework
+        const params = { courseWorkId: hw.id, courseId: hw.courseId };
+        hw.submission_request_id = submissionsBatch.add(gapi.client.classroom.courses.courseWork.studentSubmissions.list(params));
+      });
+
+      await submissionsBatch.then(batchResponse => {
+        for (const [id, response] of Object.entries(batchResponse.result)) {
+          const hw = courseWork.filter(hw => hw.submission_request_id === id)[0];
+          hw.submission = JSON.parse(response.body).studentSubmissions[0];
+        }
+      });
+    }
+
+    return {courseObj: course, hwArray: courseWork};
+  }));
+
+  detailsDiv.removeChild(progress);
+
+  courses_and_hw.forEach(course => {
+    const entry = document.createElement('details');
+
+    if (!course.hwArray) {
+      entry.innerHTML += '<p>呢個課程暫時未有任何功課</p>';
+      return;
+    }
+
+    courseInfo.push(course.hwArray);
+
+    const hwStatistics   = getCompletedPercentage(course.hwArray),
+          statisticsDiv  = document.createElement('div'),
+          summaryElement = document.createElement('summary'),
+          hwProgressBar  = createHWProgressBar(hwStatistics.percentage);
+
+    summaryElement.className = 'course';
+    statisticsDiv.className = 'statistics';
+    statisticsDiv.appendChild(hwProgressBar);
+    statisticsDiv.innerHTML += `<p style='min-width: 45px;'>${hwStatistics.completed}/${hwStatistics.total}</p>`;
+
+    summaryElement.innerHTML += `<img class='ico' src='/img/down_arrow.svg' /><p>${course.courseObj.name}</p>`;
+    summaryElement.appendChild(statisticsDiv);
+
+    entry.appendChild(summaryElement);
+
+    for (hw of course.hwArray) {
+      const hwEntry         = document.createElement('details'),
+            hwInfo          = document.createElement('div'),
+            hwInfoContainer = document.createElement('div'),
+            summary         = document.createElement('summary'),
+            hwStatus        = getHWStatus(hw),
+            badge           = generateHWStatusBadge(hwStatus);
+
+      hwInfo.appendChild(hwInfoContainer);
+      hwEntry.appendChild(hwInfo);
+
+      hwEntry.className = 'hwEntry';
+      hwInfo.className  = 'hwInfo';
+
+      console.log('Parsing homework: ', hw);
+      summary.innerHTML += `<img class='ico' src='/img/down_arrow.svg' /><p>${hw.title}</p>`;
+
+      summary.appendChild(badge);
+      hwEntry.appendChild(summary);
+
+      hwInfoContainer.innerHTML += `
+        <a href='${hw.alternateLink}'>See details on Google Classroom</a>
+        <p>詳情: </p><pre class='hwDesc'><code>${hw.description || '（冇打）'}</code></pre>
+        <p>喺 ${new Date(Date.parse(hw.creationTime)).toLocaleString()} 佈置</p>
+      `;
+
+      if (hw.materials) hwInfoContainer.innerHTML += `<p>有 ${hw.materials.length} 個附件</p>`;
+
+      if (hw.dueDate) {
+        hwInfoContainer.innerHTML += '<p>Deadline 喺 ' +
+        `${hw.dueDate.year} 年 ` +
+        `${hw.dueDate.month} 月 ` +
+        `${hw.dueDate.day} 號 ` +
+        `${addZeroPadding(hw.dueTime.hours || 0)} 點 ` +
+        `${addZeroPadding(hw.dueTime.minutes || 0)} 分</p>`;
+      }
+
+      hwEntry.innerHTML += '<hr />';
+      entry.appendChild(hwEntry);
+    }
+
+    detailsDiv.appendChild(entry);
+    detailsDiv.innerHTML += '<hr />';
+  });
+});
